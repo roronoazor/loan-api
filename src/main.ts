2dd49680585ae18f25c9ee02466f8671f1e6f633
roronoazor <ugoodumegwu@gmail.com>
2023-03-26 17:38:47 +0100

    feat: added the package.json and swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { EntityNotFoundFilter } from './exceptions/404.exception';
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
   app.useGlobalFilters(new EntityNotFoundFilter());
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+  .setTitle('Demo Loan Api')
+  .setDescription('The Demo Loan API description')
+  .setVersion('0.1')
+  .addTag('loanApi')
+  .addBearerAuth()
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(5000, () => {
+    console.log(`Application listening at port 5000`);
+  });
 }
 bootstrap();
