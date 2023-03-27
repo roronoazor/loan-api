41c9754b0dbef03bac38cdf27ea6472cf84bcf36
roronoazor <ugoodumegwu@gmail.com>
2023-03-27 09:23:35 +0100

    doc update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { 
     LoginDto,
@@ -14,12 +14,25 @@ export class AuthController {
     constructor( private authService: AuthService ){}
 
     @Post('signup')
+    @ApiOperation({
+        description: `
+         This allows the user to signup for the application, 
+         please note that the api doesn't send a mail to 
+         verify that users email.
+        `
+    })
     async signup (@Body() dto: SignupDto){
         return this.authService.signup(dto);
     }
 
     @Post('login')
     @HttpCode(200)
+    @ApiOperation({
+        description: `
+            This api allows the user to login for the application.
+            it returns user details and an access token.
+        `
+    })
     login (@Body() dto: LoginDto) {
         return this.authService.login(dto);
     }
